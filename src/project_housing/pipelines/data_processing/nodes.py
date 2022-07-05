6b56de6b5a2bef0a63f3801b67d24b78dc364fd7
AlexisNL <anoirluhalwe@yahoo.com>
2022-07-05 10:49:49 +0200

    nodes

---
+++ src/project_housing/pipelines/data_processing/nodes.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  4 19:25:22 2022
+
+@author: Alexis
+"""
+
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+   
+def _scale(x: pd.DataFrame) -> pd.DataFrame():
+    scaler = StandardScaler()
+    char = x.drop('median_income', axis=1)
+    scaler.fit_transform(char)
+
+def _dummies(housing: pd.DataFrame) -> pd.DataFrame:
+    """Preprocesses the data for housing dataframe.
+
+    Args:
+        housing: Raw data.
+    Returns:
+        Preprocessed data, with `ocean_proximity` converted to dummies
+    """
+    housing = pd.get_dummies(housing, columns = ['ocean_proximity'])
+    return housing
+
+def _scaler(housing: pd.DataFrame) -> pd.DataFrame:
+    """Preprocesses the data for cancer dataframe.
+
+    Args:
+        housing: Raw data.
+    Returns:
+        Preprocessed data, with characteristics scaled
+    """
+    model_input_table = _scale(housing)
+    return model_input_table
